6be4f07c3e3683f9ab9c5b084838c9748e67872f
RealityVibe <674788969@qq.com>
2017-06-29 09:20:11 +0800

    updateLuara

--- js/index.js
+++ js/index.js
@@ -12,31 +12,52 @@ var li=document.getElementById('lunbo').getElementsByTagName("li");
 var num=0;
 var len=li.length;
 
-setInterval(function(){
+/*setInterval(function(){
     li[num].style.display="none";
     num=++num==len?0:num;
     li[num].style.display="inline-block";
 
-},2000);//切换时间
+},2000);//切换时间*/
 
 
 // 左滑
-/* var speedpic = 2000;//速度数值越大速度越慢
-    document.getElementById("luara-list2").innerHTML = document.getElementById("luara-list1").innerHTML;
-    function Marqueepic() {
-        if (document.getElementById("luara-list2").offsetWidth
-                - document.getElementById("lunbo").scrollLeft <= 0) {
-            document.getElementById("lunbo").scrollLeft -= document
-                    .getElementById("luara-list1").offsetWidth;
-        } else {
-            document.getElementById("lunbo").scrollLeft++;
+ /*图片滚动效果*/
+
+ //速度数值越大速度越慢
+var speedpic = 3500;//换图间隔时间
+var speed2 = 10;//换图用时
+
+var flag = 1365;//换图信号
+var MyMar;
+document.getElementById("luara-list2").innerHTML = document.getElementById("luara-list1").innerHTML;
+function Mar(){
+    clearInterval(MyMarpic);
+    if (document.getElementById("luara-list2").offsetWidth
+            - document.getElementById("lunbo").scrollLeft <= 0) {
+        document.getElementById("lunbo").scrollLeft -= document
+                .getElementById("luara-list1").offsetWidth;
+    } else {
+        document.getElementById("lunbo").scrollLeft = document.getElementById("lunbo").scrollLeft + 15;
+        flag = flag -15;
+        console.log(flag);
+        if(flag <= 0){
+          // flag=1366;
+          clearInterval(MyMar);
+          MyMarpic = setInterval(Marqueepic, speedpic);
         }
     }
+}
+function Marqueepic() {
+    flag=1365;
+    MyMar = setInterval(Mar, speed2);
+    // console.log(2000);
+}
 var MyMarpic = setInterval(Marqueepic, speedpic);
-    document.getElementById("lunbo").onmouseover = function() {
-        clearInterval(MyMarpic);
-    }
-    document.getElementById("lunbo").onmouseout = function() {
-        MyMarpic = setInterval(Marqueepic, speedpic);
-    }*/
 
+/* mouseon 暂停*/    
+/*document.getElementById("lunbo").onmouseover = function() {
+    clearInterval(MyMarpic);
+}
+document.getElementById("lunbo").onmouseout = function() {
+    MyMarpic = setInterval(Marqueepic, speedpic);
+}*/
---
+++ js/webdesign.js
@@ -0,0 +1,7 @@
+/**
+ * 
+ * @authors Your Name (you@example.org)
+ * @date    2017-06-27 15:44:43
+ * @version $Id$
+ */
+
